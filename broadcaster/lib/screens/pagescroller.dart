6a2961c5f566ec6ffd08d5807a524cec87a726ee
kwesikwaa <kwesikwaahayford@gmail.com>
2022-02-06 18:51:01 +0000

    pagescroll animation done

--- broadcaster/lib/screens/pagescroller.dart
+++ broadcaster/lib/screens/pagescroller.dart
@@ -1,5 +1,6 @@
 import 'package:broadcaster/Models/toyItem.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class Pagescroller extends StatefulWidget {
    Pagescroller({Key key}) : super(key: key);
@@ -17,10 +18,32 @@ class  _PagescrollerState extends State<Pagescroller> {
     const BottomNavigationBarItem(icon: Icon(Icons.library_add),label:'zz')
   ];
   double curindex=1;
+  double indexpage=1;
   
   _listen(){
     setState(() {
       curindex = _pagecontroler.page;
+      double calc = curindex - curindex.truncateToDouble();
+
+      if(_pagecontroler.position.userScrollDirection == ScrollDirection.reverse){
+        if(calc > 0.8){
+          indexpage = curindex.ceilToDouble();
+        }
+        else{indexpage = curindex.floorToDouble();}
+      }
+      if(_pagecontroler.position.userScrollDirection == ScrollDirection.forward){
+        if(calc < 0.8){
+          indexpage = curindex.floorToDouble();
+        }
+        else{indexpage = curindex.ceilToDouble();}
+      }
+      // if(curindex > (curindex + 0.7)){
+      //   indexpage = curindex.truncateToDouble();
+      // }
+      // else {
+      //   indexpage = curindex.ceilToDouble();
+      // }
+      
     });
   }
 
@@ -70,45 +93,43 @@ class  _PagescrollerState extends State<Pagescroller> {
               itemCount: toys.length,
               controller: _pagecontroler,
               itemBuilder: (context, index){
+                // print("page ${_pagecontroler.}");
                 return Padding(
                   padding: const EdgeInsets.only(left: 10,right: 10,top: 30,bottom: 90),
-                  child: Transform.translate(
-                    offset: const Offset(0,0),
-                    child: AnimatedContainer(
-                      duration: const Duration(milliseconds: 250),
-                      curve: Curves.elasticIn,
-                      margin: EdgeInsets.only(top: index==curindex?10:30,bottom: 10),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(30)
-                        ),
-                      child: Stack(
-                        clipBehavior: Clip.none,
-                        children: [Column(
-                          children: [
-                            
-                            Expanded(
-                              child: Padding(
-                                padding: const EdgeInsets.all(20),
-                                child: Image(image: AssetImage(toys[index].image),fit: BoxFit.fill),
-                              ),),
-                            Text(toys[index].name, style: const TextStyle(fontSize: 20,fontWeight: FontWeight.w600, color: Colors.black54),),
-                            Text("GHC ${toys[index].price}", style: const TextStyle(fontSize: 32,fontWeight: FontWeight.w700, color: Colors.black54),),
-                          ],
-                        ),
-                        // Positioned(
-                        //       right: MediaQuery.of(context).size.width,
-                        //       bottom: -25,
-                        //       child: Container(
-                        //         height: 50,
-                        //         width: 50,
-                        //         decoration: BoxDecoration(
-                        //           color: Colors.blue,
-                        //           borderRadius: BorderRadius.circular(50)
-                        //         ),),
-                        //     )
-                        ]
+                  child: AnimatedContainer(
+                    duration: const Duration(milliseconds: 250),
+                    curve: Curves.bounceInOut,
+                    margin: EdgeInsets.only(top: index==indexpage?10:100,bottom: 10),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(30)
                       ),
+                    child: Stack(
+                      clipBehavior: Clip.none,
+                      children: [Column(
+                        children: [
+                          
+                          Expanded(
+                            child: Padding(
+                              padding: const EdgeInsets.all(20),
+                              child: Image(image: AssetImage(toys[index].image),fit: BoxFit.fill),
+                            ),),
+                          Text(toys[index].name, style: const TextStyle(fontSize: 20,fontWeight: FontWeight.w600, color: Colors.black54),),
+                          Text("GHC ${toys[index].price}", style: const TextStyle(fontSize: 32,fontWeight: FontWeight.w700, color: Colors.black54),),
+                        ],
+                      ),
+                      // Positioned(
+                      //       right: MediaQuery.of(context).size.width,
+                      //       bottom: -25,
+                      //       child: Container(
+                      //         height: 50,
+                      //         width: 50,
+                      //         decoration: BoxDecoration(
+                      //           color: Colors.blue,
+                      //           borderRadius: BorderRadius.circular(50)
+                      //         ),),
+                      //     )
+                      ]
                     ),
                   ),
                 );
